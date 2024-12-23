0382b1dd45b439d8b961329bbcf5e84e997453fa
arash <homayounfar.arash@gmail.com>
2024-12-23 12:21:55 +0330

    fixed group and filter

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo Jalaali',
-    'version': '18.0.1.0.0',
+    'version': '17.0.1.0.0',
     'countries': ['ir'],
     'category': 'Localization',
     'description': 'Jalaali datetime fields and datetime picker.',
@@ -16,21 +16,21 @@
     'application': False,
     'assets': {
         'web._assets_core': [
-            ('after', 'web/static/lib/luxon/luxon.js', 'odoo_jalaali/static/lib/jalaali-js.js'),
-            ('after', 'web/static/src/core/**/*', 'odoo_jalaali/static/src/core/l10n/dates_fa.js'),
-            'odoo_jalaali/static/src/core/datetime/datetime_picker_fa.js',
+            # ('after', 'web/static/lib/luxon/luxon.js', 'odoo_jalaali/static/lib/jalaali-js.js'),
+            # ('after', 'web/static/src/core/**/*', 'odoo_jalaali/static/src/core/l10n/dates_fa.js'),
+            # 'odoo_jalaali/static/src/core/datetime/datetime_picker_fa.js',
         ],
         'web.assets_backend_lazy': [
-            ('after', 'mail/static/src/views/web/activity/**', 'odoo_jalaali/static/src/mail/activity_cell.js'),
+            # ('after', 'mail/static/src/views/web/activity/**', 'odoo_jalaali/static/src/mail/activity_cell.js'),
         ],
         'web.assets_backend': [
-            'odoo_jalaali/static/src/views/remaining_days/remaining_days_field.js',
-            'odoo_jalaali/static/src/search/**/*',
+            # 'odoo_jalaali/static/src/views/remaining_days/remaining_days_field.js',
+            # 'odoo_jalaali/static/src/search/**/*',
             ],
         'web.assets_frontend': [
-            ('after', 'web/static/lib/luxon/luxon.js', 'odoo_jalaali/static/lib/jalaali-js.js'),
-            ('after', 'web/static/src/core/**/*', 'odoo_jalaali/static/src/core/l10n/dates_fa.js'),
-            'odoo_jalaali/static/src/core/datetime/datetime_picker_fa.js',
+            # ('after', 'web/static/lib/luxon/luxon.js', 'odoo_jalaali/static/lib/jalaali-js.js'),
+            # ('after', 'web/static/src/core/**/*', 'odoo_jalaali/static/src/core/l10n/dates_fa.js'),
+            # 'odoo_jalaali/static/src/core/datetime/datetime_picker_fa.js',
 
 
             # 'odoo_jalaali/static/src/core/l10n/dates_fa.js',
