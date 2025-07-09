0edd67106eaf92a4019f5cc119833ade8754bbc3
homayounfar <homayounfar@msn.com>
2025-07-09 16:34:54 +0330

    update

--- models/resource_calendar.py
+++ models/resource_calendar.py
@@ -1,19 +1,7 @@
-from odoo import models, api
+from odoo import models, api, fields
 import logging
-
-
-class JalaaliResourceCalendar(models.Model):
-    _inherit = "resource.calendar"
-
-    def _get_unusual_days(self, start_dt, end_dt, company_id=False):
-        logging.info(f"\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>[ODOO_JALAALI calendar]\n"
-                     f"{self} \n "
-                     f"{start_dt} \n "
-                     f"{end_dt} \n "
-                     f"{company_id}\n")
-        return super()._get_unusual_days(start_dt, end_dt, company_id)
-
-
+from datetime import datetime, time
+from jdatetimext import jdatejs
 
 class JalaaliHrEmployee(models.Model):
     _inherit = "hr.employee"
@@ -21,6 +9,21 @@ class JalaaliHrEmployee(models.Model):
     def _get_unusual_days(self, start_dt, end_dt,):
         logging.info(f"\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>[ODOO_JALAALI employee]\n"
                      f"{self}\n"
-                     f"{start_dt}\n"
-                     f"{end_dt} \n ")
+                     f"{start_dt}  {jdatejs(start_dt)}   {fields.Date.from_string(start_dt)}  {fields.Date.from_string(start_dt)}  {time.min}\n"
+                     f"{end_dt}  {jdatejs(end_dt)}   {fields.Date.from_string(end_dt)}  {fields.Date.from_string(end_dt)}  {time.max}\n "
+                     )
         return super()._get_unusual_days(start_dt, end_dt, )
+
+
+class JalaaliResourceCalendar(models.Model):
+    _inherit = "resource.calendar"
+
+    def _get_unusual_days(self, start_dt, end_dt, company_id=False):
+        logging.info(f"\n\n>>>>>>>>>>>>>>>>>>>>>>>>>>[ODOO_JALAALI calendar]\n"
+                     f"{self}\n "
+                     f"{start_dt}\n "
+                     f"{end_dt}\n "
+                     f"{company_id}\n"
+
+                     )
+        return super()._get_unusual_days(start_dt, end_dt, company_id)
