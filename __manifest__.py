ff4754b44760a1aa7930cd94289055daf232575b
homayounfar <homayounfar@msn.com>
2026-02-21 09:59:35 +0330

    update

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'description': 'Jalaali datetime fields and datetime picker.',
     'author': 'Arash Homayounfar',
     'website': 'https://giladoo.com/odoo_jalaali',
-    'depends': ['web', 'mail', 'hr'],
+    'depends': ['web', 'mail'],
     'external_dependencies': {
         'python': ['jdatetimext', 'khayyam'],
     },
